3db3bd1cb0c4edb9af630212e083935f781f4700
lawson <abraham.lawson.dev@gmail.com>
2021-08-13 19:32:36 +0700

    add config code push

--- android/app/src/main/java/com/rn_redux_toolkit_boilerplate/MainApplication.java
+++ android/app/src/main/java/com/rn_redux_toolkit_boilerplate/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.rollbar.RollbarReactNative;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
