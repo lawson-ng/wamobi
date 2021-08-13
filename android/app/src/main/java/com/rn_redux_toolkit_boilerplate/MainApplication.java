ea92055ec23658796592e53ba40db11a287f872a
lawson <abraham.lawson.dev@gmail.com>
2021-08-13 19:04:55 +0700

    integrate code push

--- android/app/src/main/java/com/rn_redux_toolkit_boilerplate/MainApplication.java
+++ android/app/src/main/java/com/rn_redux_toolkit_boilerplate/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -19,6 +20,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
 
         @Override
